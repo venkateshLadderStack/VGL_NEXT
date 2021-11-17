339dc9e37174c4663777810a2028da497f1d2f21
venkateshLadderStack <venkatesh@ladderstack.com>
2021-11-17 18:01:08 +0530

    added 1100 posts now

--- pages/api/fetchPosts.js
+++ pages/api/fetchPosts.js
@@ -28,26 +28,26 @@ export const fetchAllPosts = async () => {
     "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=6"
   );
   const page6posts = await page6res.json();
-  // const page7res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=7"
-  // );
-  // const page7posts = await page7res.json();
-  // const page8res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=8"
-  // );
-  // const page8posts = await page8res.json();
-  // const page9res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=9"
-  // );
-  // const page9posts = await page9res.json();
-  // const page10res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=10"
-  // );
-  // const page10posts = await page10res.json();
-  // const page11res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=11"
-  // );
-  // const page11posts = await page11res.json();
+  const page7res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=7"
+  );
+  const page7posts = await page7res.json();
+  const page8res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=8"
+  );
+  const page8posts = await page8res.json();
+  const page9res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=9"
+  );
+  const page9posts = await page9res.json();
+  const page10res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=10"
+  );
+  const page10posts = await page10res.json();
+  const page11res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=11"
+  );
+  const page11posts = await page11res.json();
 
   return [
     ...page1posts,
@@ -56,10 +56,10 @@ export const fetchAllPosts = async () => {
     ...page4posts,
     ...page5posts,
     ...page6posts,
-    // ...page7posts,
-    // ...page8posts,
-    // ...page9posts,
-    // ...page10posts,
-    // ...page11posts,
+    ...page7posts,
+    ...page8posts,
+    ...page9posts,
+    ...page10posts,
+    ...page11posts,
   ];
 };
