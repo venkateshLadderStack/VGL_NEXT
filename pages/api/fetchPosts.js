306ce41071083d9e13073241a7095b5d3c12a576
venkateshLadderStack <venkatesh@ladderstack.com>
2021-11-17 17:49:43 +0530

    noww 500 posts

--- pages/api/fetchPosts.js
+++ pages/api/fetchPosts.js
@@ -14,20 +14,20 @@ export const fetchAllPosts = async () => {
   );
   const page3posts = await page3res.json();
 
-  // const page4res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=4"
-  // );
-  // const page4posts = await page4res.json();
+  const page4res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=4"
+  );
+  const page4posts = await page4res.json();
 
-  // const page5res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=5"
-  // );
-  // const page5posts = await page5res.json();
+  const page5res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=5"
+  );
+  const page5posts = await page5res.json();
 
-  // const page6res = await fetch(
-  //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=6"
-  // );
-  // const page6posts = await page6res.json();
+  const page6res = await fetch(
+    "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=6"
+  );
+  const page6posts = await page6res.json();
   // const page7res = await fetch(
   //   "https://cms.verygoodlight.com/wp-json/wp/v2/posts?per_page=100&page=7"
   // );
@@ -53,9 +53,9 @@ export const fetchAllPosts = async () => {
     ...page1posts,
     ...page2posts,
     ...page3posts,
-    // ...page4posts,
-    // ...page5posts,
-    // ...page6posts,
+    ...page4posts,
+    ...page5posts,
+    ...page6posts,
     // ...page7posts,
     // ...page8posts,
     // ...page9posts,
