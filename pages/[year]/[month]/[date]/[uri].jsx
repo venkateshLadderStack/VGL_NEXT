d1a16fa94ed5e6a4d7c0e0aedc49537bbe19ab5d
venkateshLadderStack <venkatesh@ladderstack.com>
2021-11-18 14:40:19 +0530

    reduced to 500

--- pages/[year]/[month]/[date]/[uri].jsx
+++ pages/[year]/[month]/[date]/[uri].jsx
@@ -458,7 +458,7 @@ export async function getStaticPaths() {
     const pageposts = await pageres.json();
     bigArr = [...bigArr, ...pageposts];
     i++;
-  } while (i < 11);
+  } while (i < 6);
 
   const paths = bigArr?.map((path) => {
     const link = path.link
